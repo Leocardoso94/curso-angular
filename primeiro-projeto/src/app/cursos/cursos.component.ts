fcf6eef116bcf1fb51a5db7208298fcf26ad58c5
Leonardo <leocardosoti@gmail.com>
2017-04-20 15:57:25 -0300

    commit

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+nomePortal: string;
+
+cursos: string[] = ['Java','Angular','React'];
+
+  constructor() { 
+    this.nomePortal = 'http://loiane.training';
+
+    for (let i = 0;i < this.cursos.length; i++) {
+      var curso = this.cursos[i];      
+    }
+  }
 
   ngOnInit() {
   }
